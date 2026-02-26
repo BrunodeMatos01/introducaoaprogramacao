54812a5a5e51efeed8f4e0cc771f049193743b5f
BrunoDeMatos01 <brunodematos3@gmail.com>
2026-02-26 20:03:51 -0300

    manda

---
+++ src/MediaPonderada.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class MediaPonderada {
+    public static void main(String[] args) {
+        float prova1 = 0;
+        float prova2 = 0;
+        float projetoFinal = 0;
+        float exercicioUnidade1 = 0;
+        float exercicioUnidade2 = 0;
+        float exercicioUnidade3 = 0;
+        float exercicioUnidade4 = 0;
+        float exercicioUnidade5 = 0;
+        float exercicioUnidade6 = 0;
+        float exercicioUnidade7 = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digita a nota da prova1 ");
+        prova1 = sc.nextFloat();
+        System.out.println("Digita a nota da prova2 ");
+        prova2 = sc.nextFloat();
+        System.out.println("Digita a nota do Projeto Final ");
+        projetoFinal = sc.nextFloat();
+        System.out.println("Digita a nota da exercicioUnidade1 ");
+        exercicioUnidade1 = sc.nextFloat();
+        System.out.println("Digita a nota da exercicioUnidade2 ");
+        exercicioUnidade2 = sc.nextFloat();
+        System.out.println("Digita a nota da exercicioUnidade3 ");
+        exercicioUnidade3 = sc.nextFloat();
+        System.out.println("Digita a nota da exercicioUnidade4 ");
+        exercicioUnidade4 = sc.nextFloat();
+        System.out.println("Digita a nota da exercicioUnidade5 ");
+        exercicioUnidade5 = sc.nextFloat();
+        System.out.println("Digita a nota da exercicioUnidade6 ");
+        exercicioUnidade6 = sc.nextFloat();
+        System.out.println("Digita a nota da exercicioUnidade7 ");
+        exercicioUnidade7 = sc.nextFloat();
+
+        float MediaAritmetrica = (exercicioUnidade1 + exercicioUnidade2 + exercicioUnidade3 + exercicioUnidade4 + exercicioUnidade5 + exercicioUnidade6 + exercicioUnidade7);
+        float mediaPonderada = (prova1 * 0.2) + (prova2 * 0.3) + (projetoFinal * 0.3);
+
+    }
+
+}
