c0578d4dd0e4a9f56c49bdae49b99248eed18b6d
BrunoDeMatos01 <brunodematos3@gmail.com>
2026-02-26 19:14:37 -0300

    Removendo bin do versionamento

---
+++ src/AreaDaSalaInformada.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class AreaDaSalaInformada {
+    public static void main(String[] args) {
+        float frente = 0;
+        float lateral = 0;
+        Scanner teclado = new Scanner(System.in);
+    }
+}
