79dba5daba6e46471ba0b8ae9f1bfb96a43ad444
BrunoDeMatos01 <brunodematos3@gmail.com>
2026-04-27 21:32:49 -0300

    ok

--- aulas/src/Unidade5/Exemplo1.java
+++ aulas/src/Unidade5/Exemplo1.java
@@ -2,8 +2,21 @@ package Unidade5;
 
 public class Exemplo1 {
     public static void main(String[] args) {
-        for (int numero = 1; numero <= 10; numero++) {
+        for (int numero = 1; numero <= 100; numero++) {
             System.out.println("O num é: "+ numero);
+        }
+
+        for (int i=0; i<=100; i++){
+            System.out.println(i);
+            i++;
+
+            //if ((i % 2) == 0) {
+                //System.out.println(i);
+           //}
+
+           if ((i % 2) != 0) {
+                System.out.println(i);
+           }
         }
     }
 }
