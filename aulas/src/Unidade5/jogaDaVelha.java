1125ef9338dacc841da687715473da2e4cc8da39
BrunoDeMatos01 <brunodematos3@gmail.com>
2026-04-30 19:43:26 -0300

    ok

---
+++ aulas/src/Unidade5/jogaDaVelha.java
@@ -0,0 +1,18 @@
+package Unidade5;
+
+public class jogaDaVelha {
+    public static void main(String[] args) {
+        for (int i=0; i<=2; i++) {
+            if (i==0) {
+                System.out.println(" _ _ _");
+            }
+            for(int j=0; j<=2; j++) {
+                if (j==0) {
+                    System.out.print("|");
+                }
+                System.out.print("_|");
+            }
+            System.out.println("");
+        }
+    }
+}
