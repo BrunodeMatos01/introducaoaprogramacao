3478107754381277ed3eb39f6400fa7be2344806
BrunoDeMatos01 <brunodematos3@gmail.com>
2026-05-15 21:09:16 -0300

    ok

--- aulas/src/ExerciciosUni5/Exercicio2.java
+++ aulas/src/ExerciciosUni5/Exercicio2.java
@@ -1,5 +1,22 @@
 package ExerciciosUni5;
 
-public class Exercicio2 {
+import java.util.Scanner;
 
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int somaNumeroPar = 0;
+        int somaNumeroImpar = 0;
+        for(int i = 1; i <= 100; i++) {
+            if(i % 2 == 0) {
+                somaNumeroPar += i;
+            } else {
+                somaNumeroImpar += i;
+            }
+        }
+        System.out.println("Soma dos números pares: " + somaNumeroPar);
+        System.out.println("Soma dos números ímpares: " + somaNumeroImpar);
+    }
 }
+
